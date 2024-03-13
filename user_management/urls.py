2f5e6943f8bab2ccc6cadb943583bd7a6fcf82a3
Vern Jet Choo <choovernjet@gmail.com>
2024-03-13 22:18:14 +0800

    Profile Page base version

--- user_management/urls.py
+++ user_management/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='user_management/login.html'), name='login'),
-    path('accounts/profile/', views.user_homepage, name='user_homepage'),
+    #path('accounts/profile/', views.user_homepage, name='user_homepage'),
     path('get_all_user_data/', views.get_all_user_data, name='get_all_user_data'),
     path('user/<str:user_id>/', views.get_logged_in_user, name='get_logged_in_user'),
     path('setc/', views.show_user_images, name='setc'),
